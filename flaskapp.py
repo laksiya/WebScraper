e77ae096e784009090631209da57c010d4bfc4d0
Laksiya Balasubramaniam <36662731+laksiya@users.noreply.github.com>
2021-08-18 10:18:29 +0200

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -3,9 +3,10 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-library={}
+
 
 app = Flask(__name__)
+library={}
 @app.route('/scrape', methods=['POST'])
 def scrape():
     
@@ -14,13 +15,33 @@ def scrape():
         code = requests.get(url)
         plain = code.text
         s = BeautifulSoup(plain, 'html.parser')
+
+        text = s.find_all(text=True)
+        contents = ''
+        blacklist = [
+            '[document]',
+            'noscript',
+            'header',
+            'html',
+            'meta',
+            'head', 
+            'input',
+            'script',
+            'style'
+        ]
+
+        for t in text:
+            if t.parent.name not in blacklist:
+                contents += '{} '.format(t)
+
         info = {
                 'url':url,
                 'date':str(datetime.now()),
-                'contents': s,
+                'contents': contents,
                 'title': str(s.findAll('title'))
             }
-        newid= str(id(url))
+
+        newid= str(10000+len(library))
         library[newid]=info
         return newid
     except:
@@ -30,8 +51,11 @@ def scrape():
 def retrieve(ID):
     try:
         entry = library[str(ID)]
+        print(library[str(ID)])
+        print(type(library[str(ID)]))
+        
     except:
         entry = {}
-    return jsonify(entry)
+    return entry
 
 app.run(threaded=True)
