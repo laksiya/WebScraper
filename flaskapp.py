f1fd5a2d152c0ff874d2e30ea611d239861c0ba4
Laksiya Balasubramaniam <36662731+laksiya@users.noreply.github.com>
2021-08-18 10:25:55 +0200

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -7,45 +7,46 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 library={}
-@app.route('/scrape', methods=['POST'])
-def scrape():
-    
+@app.route('/crawl', methods=['POST'])
+def crawl():
     try:
         url = request.form['URL']
         code = requests.get(url)
-        plain = code.text
-        s = BeautifulSoup(plain, 'html.parser')
-
-        text = s.find_all(text=True)
-        contents = ''
-        blacklist = [
-            '[document]',
-            'noscript',
-            'header',
-            'html',
-            'meta',
-            'head', 
-            'input',
-            'script',
-            'style'
-        ]
-
-        for t in text:
-            if t.parent.name not in blacklist:
-                contents += '{} '.format(t)
-
-        info = {
-                'url':url,
-                'date':str(datetime.now()),
-                'contents': contents,
-                'title': str(s.findAll('title'))
-            }
-
-        newid= str(10000+len(library))
-        library[newid]=info
-        return newid
     except:
         return "Inconsistent URL - try again"
+
+    plain = code.text
+    s = BeautifulSoup(plain, 'html.parser')
+
+    text = s.find_all(text=True)
+    contents = ''
+    blacklist = [
+        '[document]',
+        'noscript',
+        'header',
+        'html',
+        'meta',
+        'head', 
+        'input',
+        'script',
+        'style'
+    ]
+
+    for t in text:
+        if t.parent.name not in blacklist:
+            contents += '{} '.format(t)
+
+    info = {
+            'url':url,
+            'date':str(datetime.now()),
+            'contents': contents,
+            'title': str(s.findAll('title'))
+        }
+
+    newid= str(10000+len(library))
+    library[newid]=info
+    return newid
+    
     
 @app.route('/retrieve/<ID>', methods=['GET'])
 def retrieve(ID):
